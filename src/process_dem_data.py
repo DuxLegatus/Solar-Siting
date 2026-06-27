a6b5bb6b26b67e661e884b27f5349ddb89cd2485
DuxLegatus <zaurimeshvildisvhili@gmail.com>
2026-06-27 17:07:01 +0400

    Downloaded and processed elevation data

---
+++ src/process_dem_data.py
@@ -0,0 +1,24 @@
+import rasterio
+import csv
+
+grid_points = []
+results = []
+with open("../data/processed/georgia_grid_points.csv", "r") as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        grid_points.append((float(row["lon"]), float(row["lat"])))
+
+with rasterio.open("../data/raw/dem/output_SRTMGL1.tif") as src:
+    for (lon, lat), val in zip(grid_points, src.sample(grid_points)):
+        results.append({
+            "latitude": lon,
+            "longitude": lat,
+            "elevation": val[0]  
+        })
+
+
+with open("../data/processed/georgia_elevation.csv", "w", newline="") as csvfile:
+    fieldnames = ["latitude","longitude","elevation"]
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerows(results)
