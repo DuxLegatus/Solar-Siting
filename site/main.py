6968fe448cd4402e6601d4aac0bb063d6d6697cd
DuxLegatus <96720149+DuxLegatus@users.noreply.github.com.>
2026-08-09 20:48:56 +0400

    added basic css

--- site/main.py
+++ site/main.py
@@ -1,46 +1,44 @@
 from fastapi import FastAPI,HTTPException,Request
+from fastapi.staticfiles import StaticFiles
 from data.sites import sites
 from fastapi.templating import Jinja2Templates
 
 
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
-
 @app.get("/")
 def home(request: Request):
     return templates.TemplateResponse(
-        {"request": request},
-        "home.html",
+        request=request,
+        name="home.html"
     )
 
 @app.get("/map")
 def map_page(request: Request):
     return templates.TemplateResponse(
-        {"request": request},
-        "georgia_suitability_map.html",
-        
+        request=request,
+        name="georgia_suitability_map.html"
     )
 
 @app.get("/findings")
 def findings(request: Request):
     return templates.TemplateResponse(
-        {
-            "request": request,
-            "sites": sites
-        },
-        "findings.html",
+        request=request,
+        name="findings.html",
+        context={"sites": sites}
     )
-
-@app.get("/sites/{site_id}")
-def get_site(site_id: int):
-    if site_id-1 in sites:
-        return sites[site_id-1]
-    raise HTTPException(status_code=404, detail="Site not found")
 
 @app.get("/about")
 def about(request: Request):
     return templates.TemplateResponse(
-        {"request": request},
-        "about.html",
+        request=request,
+        name="about.html"
     )
+
+@app.get("/sites/{site_id}")
+def get_site(site_id: int):
+    if site_id - 1 in sites:
+        return sites[site_id - 1]
+    raise HTTPException(status_code=404, detail="Site not found")
